19ade0f2132cac30a7d7473f19b542fdfdd9e69f
Bruce Lane <bruce@batchass.fr>
2016-03-18 14:47:50 +0100

    bash script added clone-addons.sh

---
+++ clone_addons.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clones dependencies for selfberry
+# Inspired by ObviousJim and ofxTimeline "clone_addons.sh" script
+
+cd ../../../addons/
+
+if [ -z $1 ]; then
+    PREFIX="git clone https://github.com/"
+else
+    PREFIX="git clone git@github.com:"
+fi
+
+${PREFIX}videodromm/ofxGifEncoder.git
+
+echo "IMPORTANT WARNING: If you're using Linux, please make sure you checkout the develop branch of ofxTextInputField"
+
+${PREFIX}jvcleave/ofxImGui.git
+
+echo "Done cloning addons! :)"
